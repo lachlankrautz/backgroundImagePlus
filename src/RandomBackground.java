f63df44c61b7a5fad83830d43da6ec847a62acce
Lachlan Krautz <lachlan.krautz@gmail.com>
2016-07-21 22:11:57 +1000

    working demo plugin

---
+++ src/RandomBackground.java
@@ -0,0 +1,31 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+
+/**
+ * Author: Lachlan Krautz
+ * Date:   21/07/16
+ */
+public class RandomBackground extends AnAction {
+
+    public RandomBackground() {
+        super("Random Background Image");
+    }
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        Project project = e.getData(PlatformDataKeys.PROJECT);
+        String txt = Messages.showInputDialog(
+                project,
+                "What is your name?",
+                "Input Your Name",
+                Messages.getQuestionIcon());
+        Messages.showMessageDialog(
+                project,
+                "Hello, " + txt + "!\n I am glad to see you.",
+                "Information",
+                Messages.getInformationIcon());
+    }
+}
